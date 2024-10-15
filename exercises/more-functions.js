f4bea63105570e74c1865a38c705fa1761d1f114
AndersonBeltran <abeltran@appdev.yearup.org>
2024-10-15 14:13:22 -0400

    functions

---
+++ exercises/more-functions.js
@@ -0,0 +1,10 @@
+"use strict";
+// function information(name, lastName, adress) {
+//   console.log(name, lastName, adress);
+// }
+// information("Anderson", "Beltran.", "Some ave APT #");
+
+function addnum(num1, num2) {
+  console.log(num1 +  " + " +  num2 + " = " + (num1 + num2));
+}
+addnum(1, 2);
---
+++ exercises/print.js
@@ -0,0 +1,9 @@
+"use strict";
+function print(message) {
+  console.log(message);
+}
+print("Hello");
+print("Does it work?");
+print("Yes it does");
+print("Nice job");
+print("Thank you");
