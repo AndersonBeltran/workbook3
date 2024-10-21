78d250f31fdc4e9811096adf90f81fe839a50d04
AndersonBeltran <abeltran@appdev.yearup.org>
2024-10-21 09:07:38 -0400

    Car rental

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,40 @@
-toll = document.getElementById("toll");
-console.log(toll.value);
+"use strict";
 
-gps = document.getElementById("gps");
-console.log(gps.value);
+//get html elements into variables
+let daysInput = document.getElementById("daysInput");
+let pickupDateInput = document.getElementById("pickupDateInput");
+let tollCheckbox = document.getElementById("tollCheckbox");
+let gpsCheckbox = document.getElementById("gpsCheckbox");
+let assistCheckbox = document.getElementById("assistCheckbox");
+let under25YesInput = document.getElementById("under25YesInput");
+let over25NoInput = document.getElementById("over25NoInput");
+let carRentalP = document.getElementById("carRentalP");
+let optionsCostP = document.getElementById("optionsCostP");
+let under25SurchargeP = document.getElementById("under25SurchargeP");
+let totalCostSpan = document.getElementById("totalCostSpan");
 
-assist = document.getElementById("assist");
-console.log(assist.value);
+// let daysInput = {
+//   type: "number",
+//   id: "daysInput",
+//   min: 1,
+//   value: "",
+// };
 
-toll.addEventListener("click", () => {
-  console.log(toll.value);
-});
+function estimateCost() {
+  //get values out of html input element
+  let numberOfDays = daysInput.value;
+  let pickupDate = pickupDateInput.value;
+  let toll = tollCheckbox.checked;
+  let gps = gpsCheckbox.checked;
+  let assist = assistCheckbox.checked;
+  let under25 = under25YesInput.checked;
+  let over25 = over25NoInput.checked;
 
-gps.addEventListener("click", () => {
-  console.log(gps.value);
-});
-
-assist.addEventListener("click", () => {
-  console.log(assist.value);
-});
+  console.log(numberOfDays);
+  console.log(pickupDate);
+  console.log(toll);
+  console.log(gps);
+  console.log(assist);
+  console.log(under25);
+  console.log(over25);
+}
